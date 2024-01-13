26bad79e236eeb7b132729659addd860325a75a8
Hyrum Perez <hyrumpa@gmail.com>
2024-01-13 18:10:04 -0500

    more changes week 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,3 @@ document.getElementById("currentYear").textContent = new Date().getFullYear();
 
 // Populate last modified date
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
