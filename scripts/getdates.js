05eeacba7f9018838b921e389b4a0bd6d672d014
Hyrum Perez <hyrumpa@gmail.com>
2024-01-13 18:02:43 -0500

    Beggining of this project

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Populate current year
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// Populate last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
