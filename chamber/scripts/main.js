14914239ce35456b099ab609f9e472b6ac9fc2ec
Hyrum Perez <hyrumpa@gmail.com>
2024-02-13 17:37:03 -0500

    Adding more pages ,card and the camber page 2 / 13

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,6 +1,6 @@
 
 const apiKey = '88676267acbf625addc2fd78c7960a55';
-const city = 'New York,us';
+const city = 'Trujillo,pe';
 
 // Current weather fetch - Mostly unchanged
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
@@ -8,6 +8,17 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
     .then(data => {
         const weatherCard = document.getElementById('weather-card');
         let weatherIcon;
+        const weatherColorMap = {
+            'Clear': '#cfe2f3',  // Light blue
+            'Clouds': '#e4e5e7', // Soft gray
+            'Rain': '#99ccff',   // Light blue
+            'Fog': '#d6d6d6',    // Light gray
+            'Thunderstorm': '#666', // Dark gray
+            'Mist': '#d6d6d6',   // Same as fog
+            'Snow': '#f0f0f0',   // Off-white
+            'default': '#f2f2f2' // Default light gray
+        };
+
 
         // Determine which weather image to use based on the weather description
         switch(data.weather[0].main) {
@@ -26,12 +37,25 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
             case 'Thunderstorm':
                 weatherIcon = '../images/stormy.png';
                 break;
+            case 'Mist':
+                weatherIcon = '../images/stormy.png';
+                break;
+            case 'Snow':
+                weatherIcon = '../images/snow.png';
+                break;
             default:
                 weatherIcon = '../images/unknown.png';
         }
 
+        const weatherCondition = data.weather[0].main;
+        const bgColor = weatherColorMap[weatherCondition] || weatherColorMap['default'];
+        const jumbotronElement = document.getElementById('jumbotron-weather');
+        if (jumbotronElement) {
+            jumbotronElement.style.backgroundColor = bgColor;
+        }
+
         weatherCard.innerHTML = `
-            <div class="card"> 
+            <div class="weather"> 
                 <div class="location">
                     <h2>${data.name}</h2>
                 </div>
