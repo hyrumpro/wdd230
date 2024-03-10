92edf7a14c433b92213518acc7e55e24ceace4fd
Hyrum Perez <hyrumpa@gmail.com>
2024-03-09 19:11:42 -0500

    Perfection and cleaning 3 / 9 More features

--- scripts/links.js
+++ scripts/links.js
@@ -3,7 +3,7 @@ const baseURL = "https://hyrumpro.github.io/wdd230/data/";
 
 
 const linksURL = baseURL + "links.json";
-
+console.log(linksURL)
 
 async function getLinks() {
     try {
@@ -12,22 +12,24 @@ async function getLinks() {
             throw new Error(`HTTP Error: ${response.status}`);
         }
         const data = await response.json();
-
         console.log(data);
-
-        displayLinks(data);
+        displayLinks(data.lessons);
     } catch (error) {
         console.error("Error fetching links:", error);
     }
 }
 
 
-function displayLinks(weeks) {
+// Updated displayLinks function
+function displayLinks(lessons) { // Rename parameter for clarity
     const linksContainer = document.querySelector(".subcard");
     linksContainer.innerHTML = "";
 
-    weeks.forEach((week) => {
-        week.links.forEach((link) => {
+    lessons.forEach((lesson) => { // Iterate lessons
+        const weekHeading = document.createElement("h3");
+        weekHeading.textContent = "Week " + lesson.lesson + ":";
+        linksContainer.appendChild(weekHeading);
+        lesson.links.forEach((link) => {
             const listItem = document.createElement("li");
             const activityLink = document.createElement("a");
             activityLink.href = baseURL + link.url;
@@ -37,6 +39,8 @@ function displayLinks(weeks) {
         });
     });
 }
+
+
 
 
 getLinks();
